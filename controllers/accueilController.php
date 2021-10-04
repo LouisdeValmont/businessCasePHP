22e07eef13d44906c1757994cc118619915f19d8
Louis de Valmont <ldevalmont@stagiaire-humanbooster.com>
2021-10-04 15:54:33 +0200

    starting block

---
+++ controllers/accueilController.php
@@ -0,0 +1,4 @@
+<?php
+
+require 'models/AccueilModel.php';
+require 'views/accueilView.php';
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+if(!empty($_GET["action"]) AND $_GET["action"]=="accueilView"):
+    include 'controllers/accueilController.php';
+endif;
