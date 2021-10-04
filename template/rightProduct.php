9c2e6e9e210176733e582511c8a517091813f505
Louis de Valmont <ldevalmont@stagiaire-humanbooster.com>
2021-10-04 16:53:01 +0200

    starting block

--- template/rightProduct.php
+++ template/rightProduct.php
@@ -3,28 +3,41 @@
     <section class="container content-section d-flex justify-content-center">
         <div class="shop-items">
             <?php
-            $i = 0;
-            foreach ($produits  as $produit): $i++; ?>
-            <div class="shop-item">
-                <p class="shop-item-title"><?php echo $produit['Titre'] ?></p>
-                <img class="shop-item-image" src="../assets/image/animals/Dogs/nourriture/Sanstitre.png">
-                <div class="shop-item-details">
-                    <span class="shop-item-price"><?php echo $produit['PrixHT'] ?>$</span>
-                    <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+            foreach ($produits as $produit):
+                if ($produit['Id_Produit'] === '5'):
+                    break;
+                endif;
+                ?>
+                <div class="shop-item">
+                    <p class="shop-item-title"><?php echo $produit['Titre'] ?></p>
+                    <img class="shop-item-image" src="../assets/image/animals/Dogs/nourriture/Sanstitre.png">
+                    <div class="shop-item-details">
+                        <span class="shop-item-price"><?php echo $produit['PrixHT'] ?>$</span>
+                        <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+                    </div>
                 </div>
-            </div>
-            <?php
-//            else:
-//            echo 'ok';
-            endforeach;
-            ?>
-
+            <?php endforeach; ?>
         </div>
     </section>
+
     <h2 class="align-items-center section-header">CHOIX DU PUBLIC</h2>
     <section class="container content-section  d-flex justify-content-center">
         <div class="shop-items">
-
+            <?php
+            foreach ($produits as $produit):
+                if ($produit['Id_Produit'] === '5'):
+                    break;
+                endif;
+                ?>
+                <div class="shop-item">
+                    <p class="shop-item-title"><?php echo $produit['Titre'] ?></p>
+                    <img class="shop-item-image" src="../assets/image/animals/Dogs/nourriture/Sanstitre.png">
+                    <div class="shop-item-details">
+                        <span class="shop-item-price"><?php echo $produit['PrixHT'] ?>$</span>
+                        <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+                    </div>
+                </div>
+            <?php endforeach; ?>
         </div>
     </section>
 </div>
